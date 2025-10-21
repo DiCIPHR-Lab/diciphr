4722472b69adf2e52d19301748a3c3f41b1bf66a
DiCIPHR Lab <diciphrlab@gmail.com>
2025-10-21 13:50:31 -0400

    Add files via upload
    added copy_json option

--- diciphr/scripts/reorient_nifti.py
+++ diciphr/scripts/reorient_nifti.py
@@ -1,9 +1,9 @@
 #! /usr/bin/env python
 
-import os, sys, logging
-from diciphr.utils import check_inputs, make_dir, protocol_logging, DiciphrArgumentParser, DiciphrException
-from diciphr.nifti_utils import read_nifti, write_nifti, read_dwi, write_dwi, reorient_dwi, reorient_nifti
-import nibabel as nib
+import os, sys, shutil, logging
+from diciphr.utils import check_inputs, make_dir, protocol_logging, DiciphrArgumentParser
+from diciphr.nifti_utils import ( read_nifti, write_nifti, read_dwi, write_dwi, 
+                                 strip_nifti_ext, reorient_dwi, reorient_nifti )
 
 DESCRIPTION = '''
     Reorient a Nifti volume to lab-default LPS orientation. 
@@ -13,18 +13,22 @@ PROTOCOL_NAME='Reorient_Nifti'
     
 def buildArgsParser():
     p = DiciphrArgumentParser(description=DESCRIPTION)
-    p.add_argument('-i', '-d', action='store',metavar='datafile',dest='datafile',
+    p.add_argument('-i', '-d', action='store', metavar='datafile', dest='datafile',
                     type=str, required=True, 
                     help='Input filename'
                     )
-    p.add_argument('-o',action='store',metavar='outputfile',dest='outputfile',
+    p.add_argument('-o', action='store', metavar='outputfile', dest='outputfile',
                     type=str, required=True, 
                     help='Output filename'
                     )
-    p.add_argument('-r',action='store',metavar='orn_string',dest='orn_string',
+    p.add_argument('-r', action='store', metavar='orn_string', dest='orn_string',
                     type=str, required=False, default='LPS', 
                     help='Orientation string. Default LPS'
                     )
+    p.add_argument('-j', '--copy-json', action='store_true', dest='copy_json', 
+                    required=False, 
+                    help='Copy any associated .json file to output'
+                   )
     return p
     
 def main(argv):
@@ -36,12 +40,12 @@ def main(argv):
     try:
         check_inputs(args.datafile, nifti=True)
         check_inputs(output_dir, directory=True)
-        run_reorient_nifti(args.datafile, args.outputfile, orn_string=args.orn_string)
+        run_reorient_nifti(args.datafile, args.outputfile, orn_string=args.orn_string, copy_json=args.copy_json)
     except Exception:
-        logging.exception(f"Exception encountered running {PROTOCOL_NAME}")
+        logging.exception(f'Exception encountered running {PROTOCOL_NAME}')
         raise
     
-def run_reorient_nifti(datafile, outputfile, orn_string='LPS'):
+def run_reorient_nifti(datafile, outputfile, orn_string='LPS', copy_json=False):
     ''' 
     Run the DTI Preprocessing protocol.
     
@@ -52,39 +56,50 @@ def run_reorient_nifti(datafile, outputfile, orn_string='LPS'):
     outputfile : str
         Target labels file from freesurfer_postprocess
     orn_string : Optional[str]
-        Orientation string of 
+        Orientation string 
+    copy_json : Optional[bool]
+        Copy associated .json file to output
     Returns
     -------
     None
     '''
-    logging.info('datafile: {}'.format(datafile))
-    logging.info('outputfile: {}'.format(outputfile))
-    logging.info('orn_string: {}'.format(orn_string))
+    logging.info(f'datafile: {datafile}')
+    logging.info(f'outputfile: {outputfile}')
+    logging.info(f'orn_string: {orn_string}')
     
-    logging.info('Begin Protocol {}'.format(PROTOCOL_NAME))    
+    logging.info(f'Begin Protocol {PROTOCOL_NAME}')
     # Load datafile
     logging.info('Read input nifti')
     diffusion=False
     try:
         dwi_im, bvals, bvecs = read_dwi(datafile)
         diffusion=True
-        logging.info('Diffusion volume detected.')
+        logging.info('Diffusion volume detected')
     except:
         nifti_im = read_nifti(datafile)
 
     # Output filenames 
     if diffusion:
-        logging.info('Reorienting diffusion volume.')
+        logging.info('Reorienting diffusion volume')
         dwi_reor_im, bvals_reor, bvecs_reor = reorient_dwi(dwi_im, bvals, bvecs, orientation=orn_string)
-        logging.info('Saving to file {}.'.format(outputfile))
+        logging.info(f'Saving to file {outputfile}')
         write_dwi(outputfile, dwi_reor_im, bvals_reor, bvecs_reor) 
     else:
-        logging.info('Reorienting Nifti volume.')
+        logging.info('Reorienting Nifti volume')
         nifti_reor_im = reorient_nifti(nifti_im, orientation=orn_string)
-        logging.info('Saving to file {}.'.format(outputfile))
+        logging.info(f'Saving to file {outputfile}')
         write_nifti(outputfile, nifti_reor_im)
     
-    logging.info('End of Protocol {}'.format(PROTOCOL_NAME))
+    if copy_json:
+        logging.info('Copy associated .json files')
+        jsonfile = strip_nifti_ext(datafile)+'.json'
+        target_jsonfile = strip_nifti_ext(outputfile)+'.json'
+        if os.path.exists(jsonfile):
+            shutil.copyfile(jsonfile, target_jsonfile)
+        else:
+            logging.warning(f'JSON file does not exist: {jsonfile}')
+    
+    logging.info(f'End of Protocol {PROTOCOL_NAME}')
     
 if __name__ == '__main__': 
     main(sys.argv[1:])
