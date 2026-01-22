d78cda6ab84c8677cc046d6413b6cc82ac6ef236
DiCIPHR Lab <diciphrlab@gmail.com>
2026-01-22 16:55:42 -0500

    Add files via upload

--- diciphr/fernet/pipeline.py
+++ diciphr/fernet/pipeline.py
@@ -1,10 +1,12 @@
 import os
 import logging
+import warnings
 import numpy as np
 import nibabel as nib
 from dipy.reconst import dti
 from dipy.core.gradients import gradient_table_from_bvals_bvecs
 from diciphr.nifti_utils import read_dwi, write_dwi, nifti_image
+from diciphr.diffusion import n4_bias_correct_dwi, has_gaussian_shells, extract_gaussian_shells 
 from diciphr.fernet.utils import erode_mask 
 from diciphr.fernet.free_water import grad_data_fit_tensor, clip_tensor_evals 
 
@@ -57,6 +59,21 @@ def tissue_rois(mask, fa, tr, erode_iterations=10, fa_threshold=0.7, tr_threshol
     csf_roi = np.logical_and(mask, tr>tr_threshold)
     return wm_roi, csf_roi
 
+def gradient_table_no_bzero(bvals, bvecs):
+    '''
+    Runs dipy gradient_table_from_bvals_bvecs while suppressing warnings about b0_threshold
+    '''
+    with warnings.catch_warnings():
+        warnings.filterwarnings(
+            "ignore",
+            message=r".*b0_threshold.*is too low.*",
+            category=UserWarning,
+            module=r"dipy\.core\.gradients"
+        )
+        gtab = gradient_table_from_bvals_bvecs(bvals, bvecs)
+    return gtab 
+
+
 def initial_fit(dwi, bvals, bvecs, mask, wm_roi, csf_roi, MD, 
         csf_percentile=95, wm_percentile=5, lmin=0.1e-3, lmax=2.5e-3, 
         evals_lmin=0.1e-3, evals_lmax=2.5e-3, md_value=0.6e-3, 
@@ -67,12 +84,12 @@ def initial_fit(dwi, bvals, bvecs, mask, wm_roi, csf_roi, MD,
     logging.info("Compute baseline image and DW attenuation.")
     dim_x, dim_y, dim_z = mask.shape
     indices_dwi = (bvals > 0)
-    nb_dwi = np.count_nonzero(indices_dwi)
     indices_b0 = (bvals == 0)
-    nb_b0 = np.count_nonzero(indices_b0)  
     b = bvals.max()    
     b0 = dwi[..., indices_b0].mean(-1)
-    signal = dwi[..., indices_dwi] / b0[..., None]
+    # suppress expected divide by zero warning 
+    with np.errstate(divide='ignore', invalid='ignore'):
+        signal = dwi[..., indices_dwi] / b0[..., None]
     np.clip(signal, 1.0e-6, 1-1.0e-6, signal)
     signal[np.logical_not(mask)] = 0.
     csf_b0 = np.percentile(b0[csf_roi], csf_percentile) 
@@ -82,7 +99,9 @@ def initial_fit(dwi, bvals, bvecs, mask, wm_roi, csf_roi, MD,
 
     logging.info("Compute initial volume fraction ..." )
     epsi = 1e-12 # to prevent log(0)
-    init_f = 1 - np.log(b0/wm_b0 + epsi)/np.log(csf_b0/wm_b0)
+    # suppress expected invalid value in log warning 
+    with np.errstate(invalid='ignore'):
+        init_f = 1 - np.log(b0/wm_b0 + epsi)/np.log(csf_b0/wm_b0)
     np.clip(init_f, 0.00, 1.00, init_f)
     alpha = init_f.copy()
 
@@ -127,7 +146,7 @@ def initial_fit(dwi, bvals, bvecs, mask, wm_roi, csf_roi, MD,
                      / (init_f[..., np.newaxis])
     np.clip(corrected_signal, 1.0e-3, 1.-1.0e-3, corrected_signal)
     log_signal = np.log(corrected_signal)
-    gtab = gradient_table_from_bvals_bvecs(bvals, bvecs)
+    gtab = gradient_table_no_bzero(bvals, bvecs)
     H = dti.design_matrix(gtab)[:, :6]
     pseudo_inv = np.dot(np.linalg.inv(np.dot(H.T, H)), H.T)
     init_tensor = np.dot(log_signal, pseudo_inv.T)
@@ -152,16 +171,15 @@ def gradient_descent(dwi, bvals, bvecs, mask, init_f, init_tensor, niters=50, we
     '''
     dim_x, dim_y, dim_z = mask.shape
     indices_dwi = (bvals > 0)
-    nb_dwi = np.count_nonzero(indices_dwi)
     indices_b0 = (bvals == 0)
-    nb_b0 = np.count_nonzero(indices_b0)  
-    b = bvals.max()    
     b0 = dwi[..., indices_b0].mean(-1)
-    signal = dwi[..., indices_dwi] / b0[..., None]
+    # suppress expected divide by zero warning 
+    with np.errstate(divide='ignore', invalid='ignore'):
+        signal = dwi[..., indices_dwi] / b0[..., None]
     np.clip(signal, 1.0e-6, 1-1.0e-6, signal)
     bvals = bvals[indices_dwi]
     bvecs = bvecs[indices_dwi]
-    gtab = gradient_table_from_bvals_bvecs(bvals, bvecs)
+    gtab = gradient_table_no_bzero(bvals, bvecs)
     H = dti.design_matrix(gtab)[:, :6]
     signal[np.logical_not(mask)] = 0.
     signal = signal[mask]
@@ -188,7 +206,6 @@ def gradient_descent(dwi, bvals, bvecs, mask, init_f, init_tensor, niters=50, we
         volume_fraction -= weight * step_size * gradf
 
         grad1[np.isnan(grad1)] = 0
-        # np.clip(grad1, -1.e5, 1.e5, grad1)
         np.clip(volume_fraction, 0.01, 0.99, volume_fraction)
         lower_triangular -= step_size * grad1
         lower_triangular[np.isnan(lower_triangular)] = 0
@@ -211,19 +228,27 @@ def run_fernet(dwi_filename, bvals_filename, bvecs_filename, mask_filename, outp
                 wm_roi=None, csf_roi=None, exclude_mask=None, interpolate=True, fixed_MD=False, 
                 erode_iterations=8, fa_threshold=0.7, tr_threshold=0.0085, wm_percentile=5, csf_percentile=95, 
                 md_value=0.6e-3, lmin=0.1e-3, lmax=2.5e-3, evals_lmin=0.1e-3, evals_lmax=2.5e-3, 
-                niters=50, weight=100.0, step_size=1.0e-7
+                niters=50, weight=100.0, step_size=1.0e-7, gaussian=True, bias_correct=False
                 ):
     output_basename = os.path.realpath(output_basename)
     
     logging.info("Read DWIs from disk...")
     dwi_img, bvals, bvecs = read_dwi(dwi_filename, bvals_filename, bvecs_filename)
+    logging.info("Read mask image from disk...")
+    mask_img = nib.load(mask_filename)
+    
+    if gaussian and not has_gaussian_shells(bvals):
+        raise ValueError("FERNET cannot be run on datasets with no b-values in the range 500-1500 s/mm2")
+    if gaussian:
+        dwi_img, bvals, bvecs = extract_gaussian_shells(dwi_img, bvals, bvecs)
+    if bias_correct:
+        dwi_img, bvals, bvecs = n4_bias_correct_dwi(dwi_img, bvals, bvecs, mask_img=mask_img)
+    
     bvals = bvals.flatten()
     bvecs = bvecs.transpose()
     affine = dwi_img.affine
     dwi = dwi_img.get_fdata()
 
-    logging.info("Read mask image from disk...")
-    mask_img = nib.load(mask_filename)
     mask = np.asarray(mask_img.get_fdata(), dtype=bool)
     dwi[np.logical_not(mask),...] = 0 
     
